e5e5d721c5ce061c41f4a5ad2644c3c82e7b444e
cyrilweller <cyril.weller@protonmail.com>
2016-10-18 18:15:53 +0200

    Created empty Java project

---
+++ src/main/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author cyril
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
