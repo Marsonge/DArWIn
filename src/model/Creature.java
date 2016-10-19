076fca032f2363871afb369f0f427f26edb26ce5
Fabien Bal-Fontaine <fabien_bf@hotmail.fr>
2016-10-19 14:42:26 +0200

    Creature : added some new particularities
    random move, speed (5), eat (does nothing except increment foodLevel)

--- src/model/Creature.java
+++ src/model/Creature.java
@@ -1,16 +1,20 @@
 package model;
 
+import java.util.Random;
+
 public class Creature {
 	private int id;
 	private int x;
 	private int y;
 	private int foodLevel;
+	private int speed;
 	
 	public Creature(int id, int x, int y){
 		this.id = id;
 		this.x = x;
 		this.y = y;
 		this.foodLevel = 50;
+		this.speed = 2;
 	}
 
 	public int getId() {
@@ -37,6 +41,36 @@ public class Creature {
 		this.y = y;
 	}
 	
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public boolean eat(){
+		foodLevel++;
+		return true;
+	}
 	
+	public boolean move(){
+		//TODO : Implement a better move?
+		Random rand = new Random();
+		switch(rand.nextInt(4)){
+			case 0:
+				x-=speed;
+				break;
+			case 1:
+				x+=speed;
+				break;
+			case 2:
+				y-=speed;
+				break;
+			case 3:
+				y+=speed;
+		}
+		return true;
+	}
 	
 }
