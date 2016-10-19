93dc7a9d6f0df0fe093b5b50085141f2e27b6b95
cyrilweller <cyril.weller@protonmail.com>
2016-10-19 19:37:43 +0200

    Added Start/Pause button

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -3,7 +3,10 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
@@ -15,8 +18,10 @@ import model.grid.Grid;
 public class MainView extends JFrame {
 	
 	public static final Color black = new Color(0,0,0);
-	private final Timer timer;
+	private Timer timer;
 	private int tick;
+	WorldControler wc ; 
+	ViewGrid vG ;
 	
 	/**
 	 * MainView()
@@ -25,10 +30,10 @@ public class MainView extends JFrame {
 	 * @throws InterruptedException 
 	 * 
 	 */
-	public MainView() throws InterruptedException{
+	public MainView() throws InterruptedException {
 		
 		// Create the main JFrame
-		JFrame mainFrame = new JFrame("Darwin : ARtificial Wildlife Intelligence");
+		JFrame mainFrame = new JFrame("Darwin : ARtificial Wildlife INtelligence");
         mainFrame.setResizable(false);
 
         // main JFrame setting
@@ -40,18 +45,48 @@ public class MainView extends JFrame {
         mainFrame.setLayout(new BorderLayout());
 		
 		// Create the grid
-		WorldControler wc = new WorldControler(100,7,(float)10000,0,60); 
-        ViewGrid vG = new ViewGrid(wc);
-        
+		this.wc = new WorldControler(100,7,(float)10000,0,60); 
+    	this.vG = new ViewGrid(wc);
+    	
+    	this.timer = new Timer(tick, new TimerActionListener(wc));
         
         JPanel jpanel = new JPanel();
         jpanel.setPreferredSize(new Dimension(500,700)); 
         jpanel.setBackground(black);
+
+        // Start Button
+        JButton start = new JButton("Start");
+        
+        
+        // When start is clicked, simulation start and button displays stop
+        // When it is clicked on stop, simulation pauses
+        start.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)
+            {
+                Object source = e.getSource();
+                if (source instanceof JButton) {
+                	
+                    JButton btn = (JButton)source;
+                    
+                    if(btn.getText().equals("Start")){
+                        timer.start();
+                        btn.setText("Pause");
+
+                    } else{
+                    	timer.stop();
+                    	btn.setText("Start");
+                    }
+
+                }
+            }
+        });
+        
+        // Start button added to JPanel
+        jpanel.add(start);
         
         // Adding viewGrid to mainFrame
-        mainFrame.add(vG, BorderLayout.WEST);
         mainFrame.add(jpanel, BorderLayout.EAST);
-        
+        mainFrame.add(vG, BorderLayout.WEST);
         mainFrame.pack();
         mainFrame.setVisible(true);
 
@@ -60,9 +95,9 @@ public class MainView extends JFrame {
         
         // Start main timer
         this.tick = 100;
-        this.timer = new Timer(tick, new TimerActionListener(wc));
-        this.timer.start();
+        this.timer = new Timer(tick, new TimerActionListener(wc));  
 	}
+	
 	
 	/**
 	 * main function of the project, will create the view
