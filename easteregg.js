48edeb1ae7329a0c2b2e25a3ddbe941f0b3daa80
Tim "Docteur" Caillot <tim.caillot@gmail.com>
2016-11-24 18:56:34 +0100

    Update easteregg.js

--- easteregg.js
+++ easteregg.js
@@ -1,4 +1,4 @@
 var easter_egg = new Konami(function() {
       var video = document.querySelector("iframe"); 
-      video.setAttribute("src", "https://www.youtube.com/embed/Akwm2UZJ34o");
+      video.setAttribute("src", "https://www.youtube.com/embed/Akwm2UZJ34o?autoplay=1");
       });
